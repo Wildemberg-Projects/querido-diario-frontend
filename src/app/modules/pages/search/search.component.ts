eff5380ce9aea10b6f98dc72f7ea34722b427c2e
Cristian Furtado <csafurtado.dev@gmail.com>
2024-04-02 17:08:41 -0300

    Adiciona funcionalidade de download de CSVs dos enxertos clicados
    Co-authored-by: pkbceira03 <cabeceira2003@gmail.com>

--- src/app/modules/pages/search/search.component.ts
+++ src/app/modules/pages/search/search.component.ts
@@ -11,6 +11,9 @@ import { LevelDescription, Pagination, SearchResponse } from 'src/app/interfaces
 import { Territory } from 'src/app/interfaces/territory';
 import { GazetteService } from 'src/app/services/gazette/gazette.service';
 import { TerritoryService } from 'src/app/services/territory/territory.service';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
+
+var data = Array();
 
 @Component({
   selector: 'app-search',
@@ -178,11 +181,31 @@ export class SearchComponent implements OnInit {
     return text.replace('\n', '<br />');
   }
 
-  selectExcerpts(text: string, data: string, territory: string) {
+  selectExcerpts(text: string, date: string, territory: string, state: string, url: string) {
     text = this.formatText(text);
 
-    console.log(text);
+    let selected={
+      'URL': url,
+      'excerpt': text,
+      'date-published': date,
+      'location': territory+" ("+state+")",
+    }
+
+    console.log(selected);
+
+    data.push(selected);
+
     console.log(data);
-    console.log(territory);
+
+    //console.log(this.gazetteResponse?.gazettes[0].downloads);
+  }
+
+  downloadCSV() {
+    var options = {
+      showLabels: true,
+      headers: ["Link para Download do Diário Oficial", "Excerto", "Data de Publicação", "Local"]
+    }
+
+    new ngxCsv(data, 'myExcerpts', options);
   }
 }
